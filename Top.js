b1508bf98b15c787e14572a95ce0b3f947cdbb76
mitoriyuuka <y-mitori@care-con.co.jp>
2023-06-20 10:11:42 +0900

    Topレスポンシブまでの保存

---
+++ Top.js
@@ -0,0 +1,21 @@
+var hamburgerElements = document.querySelectorAll(".hamburger");
+hamburgerElements.forEach(function (element) {
+    element.addEventListener("click", function () {
+        this.classList.toggle("open");
+    });
+});
+
+document.querySelector(".hamburger").addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.querySelector("#g-nav").classList.toggle('panelactive');
+    document.querySelector(".circle-bg").classList.toggle('circleactive');
+});
+
+document.querySelectorAll("#g-nav a").forEach(function (link) {
+    link.addEventListener('click', function () {
+        document.querySelector(".hamburger").classList.remove('open');
+        document.querySelector(".hamburger").classList.remove('active');
+        document.querySelector("#g-nav").classList.remove('panelactive');
+        document.querySelector(".circle-bg").classList.remove('circleactive');
+    });
+});
